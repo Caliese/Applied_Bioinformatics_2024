3331fb98bfc632ccabc841fa2e4d5dec0aa13fbe
Caliese <135731149+Caliese@users.noreply.github.com>
2024-09-09 09:40:56 -0400

    Add files via upload

--- First_program.R
+++ First_program.R
@@ -14,5 +14,13 @@ boxplot(data)
 #plot with labels
 boxplot(data, xlab="Samples", ylab= "Genes", main= "Raw Microarray Plot", col="pink")
 
+#Perform normalization on raw datasets
+#Explain what is normalization
+#Normalization does not affect the spread of the data, the min or max values
+data_norm <- rma(data)
 
+#To extract (gene) expression values
+data_norm <- exprs(data_norm)
 
+#Plot the normalized dataset
+boxplot(data_norm, xlab="Samples", ylab= "Genes", main= "Normalized Microarray Plot", col="green")
